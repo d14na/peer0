7fd4bc14ce997480f07194d24c516b4ac965f7c2
D14na Support <support@d14na.org>
2018-06-30 15:59:47 -0400

    Added gulp testing.

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-exports.test = function () {
-    console.log('This is a test from Peer Zero')
-}
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('ci with travis')
+})
+
+const server = app.listen(3000, () => {
+    console.log('App running on port 3000')
+})
+
+module.exports = server
