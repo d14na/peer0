e316089207ab49bc442498c6a063c00fe3911efe
D14na Support <support@d14na.org>
2018-06-30 16:02:17 -0400

    Simple peer zero test.

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-const express = require('express')
-
-const app = express()
-
-app.get('/', (req, res) => {
-    res.send('ci with travis')
-})
-
-const server = app.listen(3000, () => {
-    console.log('App running on port 3000')
-})
-
-module.exports = server
+exports.test = function () {
+    console.log('This is a test from Peer Zero')
+}
