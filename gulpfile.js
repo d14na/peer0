5626533afb01d3514c06f0b810817100b79e93d7
D14na Support <support@d14na.org>
2018-07-01 06:35:54 -0400

    Added codecov report submission.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
-var gulp = require('gulp')
+const codecov = require('gulp-codecov')
+const gulp = require('gulp')
 
 gulp.task('default', function() {
     // place code for your default task here
 })
+
+gulp.src('./coverage/lcov.info').pipe(codecov())
