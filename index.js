27f6acfcdfa28e87f71f8994fa056f4cbd0afbea
D14na Support <support@d14na.org>
2018-06-30 22:22:47 -0400

    Sampling new functions.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-exports.test = function () {
-    console.log('This is a log test by Peer Zero')
-    return 'Hi there! My name is Peer Zero'
+exports.hello = function () {
+    console.log('Hi! This is a sample log message from Peer Zero.')
+    return 'Hi there! My name is Peer Zero. Would you like to play a game?'
+}
+
+exports.triple = function (val) {
+    return val * 3
 }
